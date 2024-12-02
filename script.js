477c0c8a1af5e717fe806f4958ff248de9938998
JeetrK <526HKlein@frhsd.com>
2024-12-02 15:59:16 +0000

    fixed one of the java inputs

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let verbiy = prompt("name another verb");
 let adjectivel = prompt("name another adjective");
 let verbss = prompt("name another verb");
 
-let mystory = `<h1> ${username} was sitting at their computer, feeling ${adjective} as they tried to debug a [adjective] line of code. Suddenly, they realized they had forgotten to add a ${noun} , causing the program to ${verb}. After hours of ${verbing} , they decided to ${verbiy} their computer in frustration. Just when they were about to give up, a ${adjectivel} error message appeared, and ${username}  couldnt help but ${verbss} with excitement. </h1>`;
+let mystory = `<h1> ${username} was sitting at their computer, feeling ${adjective} as they tried to debug a ${adjective} line of code. Suddenly, they realized they had forgotten to add a ${noun} , causing the program to ${verb}. After hours of ${verbing} , they decided to ${verbiy} their computer in frustration. Just when they were about to give up, a ${adjectivel} error message appeared, and ${username}  couldnt help but ${verbss} with excitement. </h1>`;
 
 console.log(mystory);
 
